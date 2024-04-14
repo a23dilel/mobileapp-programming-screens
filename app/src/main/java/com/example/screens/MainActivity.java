bb4d663f92cb43d500dafcd9d03887fc1d6311b5
a23dilel <a23dilel@student.his.se>
2024-04-14 21:37:14 +0200

    Added a button functionality in MainActivity.java for listening and send intent data.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // created a button
+        Button button = findViewById(R.id.button);
+
+        // listening for when user clicked a button
+        button.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v){
+
+                // send intent value to SecondActivity
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("name", "Charlie");
+                startActivity(intent);
+            }
+        });
     }
 }
